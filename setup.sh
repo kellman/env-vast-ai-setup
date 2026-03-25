478c940bf9bb96d68021760f216aae7b8729769e
Michael Kellman <kellmanmr@gmail.com>
2026-03-25 20:26:33 +0000

    update

--- setup.sh
+++ setup.sh
@@ -22,14 +22,6 @@ curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-
 tar -xf $ROOT/vscode_cli.tar.gz
 rm $ROOT/vscode_cli.tar.gz
 
-# Install GitHub CLI
-apt-get install -y gh
-gh auth login
-
-# Configure github stuff
-git config --global user.email "kellmanmr@gmail.com"
-git config --global user.name "Michael Kellman"
-
 # Verify installations
 echo "Verifying installations..."
 nvidia-smi
@@ -45,5 +37,14 @@ uv pip install -r $ROOT/$REPO/uv/requirements.txt
 git clone https://github.com/kellman/deepul.git
 unzip -qq $ROOT/deepul/homeworks/hw1/data/hw1_data.zip -d $ROOT/deepul/homeworks/hw1/data/
 pip install $ROOT/deepul
+
+# Install GitHub CLI
+apt-get install -y gh
+gh auth login
+
+# Configure github stuff
+git config --global user.email "kellmanmr@gmail.com"
+git config --global user.name "Michael Kellman"
+
 
 echo "Provisioning complete!"
